9fe8c6eab563099e3c10cfcafe4264e28f2aa7cd
Minnowfeather <Minnowfeather@github.com>
2023-03-17 11:40:49 -0700

    Added compile and run scripts for linux

---
+++ c.sh
@@ -0,0 +1,1 @@
+javac --module-path $PATH_TO_FX --add-modules javafx.media,javafx.swing -d bin/ -cp src src/furRhythm_recoded/*.java
---
+++ r.sh
@@ -0,0 +1,1 @@
+java --module-path $PATH_TO_FX --add-modules javafx.swing,javafx.media -cp bin furRhythm_recoded/Main
